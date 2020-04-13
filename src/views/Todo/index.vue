9980758eb06d8a176609bfb340e5ad5e40f392ff
mnao305 <naomasa305@gmail.com>
2020-04-13 22:50:56 +0900

    不要なものを消してTodoページの土台を設置

---
+++ src/views/Todo/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Todo予定地
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+// defineComponentを使うことで型推論が効くようになるらしい
+export default defineComponent({
+  setup () {
+    console.log('hello world')
+  }
+})
+</script>

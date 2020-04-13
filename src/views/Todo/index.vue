9cc306650a1926885df4ea44cb157d27d4fccb8d
mnao305 <naomasa305@gmail.com>
2020-04-14 00:34:02 +0900

    コメント追加

--- src/views/Todo/index.vue
+++ src/views/Todo/index.vue
@@ -8,6 +8,7 @@
 import { defineComponent } from '@vue/composition-api'
 
 // defineComponentを使うことで型推論が効くようになるらしい
+// composition-apiv0.4.0でcreateComponentからdefineComponentに名前が変わった
 export default defineComponent({
   setup () {
     console.log('hello world')
